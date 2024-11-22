3f63e93691f7e3237710a1e826735ad0224a6971
hiroki <haohuirixia851@gmail.com>
2024-11-22 16:51:42 +0900

    add SearchQuery

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,6 +5,7 @@ import { Form, FormControl, FormField, FormItem } from "./ui/form";
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { useEffect } from "react";
 
 const formSchema = z.object({
   searchQuery: z.string({
@@ -18,12 +19,18 @@ type Props = {
   onSubmit: (formData: SearchForm) => void;
   placeHolder: string;
   onReset?: () => void;
+  searchQuery?: string;
 };
 
-const SearchBar = ({ onSubmit, placeHolder, onReset }: Props) => {
+const SearchBar = ({ onSubmit, placeHolder, onReset, searchQuery }: Props) => {
   const form = useForm<SearchForm>({
     resolver: zodResolver(formSchema),
+    defaultValues: { searchQuery },
   });
+
+  useEffect(() => {
+    form.reset({ searchQuery });
+  }, [form, searchQuery]);
 
   const handleReset = () => {
     form.reset({
@@ -65,16 +72,14 @@ const SearchBar = ({ onSubmit, placeHolder, onReset }: Props) => {
           />
         </div>
         <div className="flex flex-row gap-3">
-          {form.formState.isDirty && (
-            <Button
-              onClick={handleReset}
-              type="button"
-              variant="outline"
-              className="rounded-full"
-            >
-              Clear
-            </Button>
-          )}
+          <Button
+            onClick={handleReset}
+            type="button"
+            variant="outline"
+            className="rounded-full"
+          >
+            Reset
+          </Button>
           <Button type="submit" className="rounded-full bg-orange-500">
             Search
           </Button>
